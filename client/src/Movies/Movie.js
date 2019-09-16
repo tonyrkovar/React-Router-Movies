71d18b0e28bf7cb89ec5f8890e7615435c0607c5
Tony <tonyrkovar@gmail.com>
2019-09-16 15:17:20 -0500

    got the project completed by routing to the appropriate states(?) and got the movies to link to the appropriate place as well as getting my code DRY by creating a moviecard component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
- 
+  console.log(props)
+  const [movie, setMovie] = useState();
+  const id = props.match.params.id;
+
   useEffect(() => {
-    const id = 1;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,7 +20,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -30,25 +32,10 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard key={movie.id} movie={movie} />
       <div className="save-button">Save</div>
     </div>
   );
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const MovieCard = props => {
-  return;
+  console.log('this is in moviecard',props);
+  return(
+    <div className="movie-card">
+      <h2>{props.movie.title}</h2>
+      <div className="movie-director">
+        Director: <em>{props.movie.director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{props.movie.metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {props.movie.stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+      <br />
+      
+      <Link to={`/movies/${props.movie.id}`}>Read More About {props.movie.title}</Link>
+    </div>
+  )
 };
 
 export default MovieCard;
